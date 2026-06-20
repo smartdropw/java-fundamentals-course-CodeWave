09577759038c755cc996c887e00e492c00f62fb8
CamilaaAlizee <mykokomi707@gmail.com>
2026-06-19 20:54:56 -0500

    add video 2 code

---
+++ completed-examples/Leccion2_VariablesOperadores/VariablesOperadores.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creación de variables de texto y numéricas
+        String nombre = "Alex";
+        int edad = 15;
+        
+        // Uso de operadores para calcular una expresión
+        int puntosIniciales = 100;
+        int bono = 50;
+        int puntosTotales = puntosIniciales + bono;
+        
+        // Instrucciones para mostrar los resultados combinados en la consola
+        System.out.println("Jugador: " + nombre);
+        System.out.println("Edad: " + edad + " años");
+        System.out.println("Puntos totales: " + puntosTotales);
+    }
+}
