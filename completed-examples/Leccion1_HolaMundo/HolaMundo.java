64dd67e926ca356cf1879a4ed63cb4f058db39c8
CamilaaAlizee <mykokomi707@gmail.com>
2026-06-19 20:53:57 -0500

    add video 1 code

---
+++ completed-examples/Leccion1_HolaMundo/HolaMundo.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Nuestra primera instrucción para mostrar texto en la consola
+        System.out.println("¡Hola, Mundo!");
+    }
+}
